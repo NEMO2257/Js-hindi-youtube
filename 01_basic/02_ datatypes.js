4b31023984f68a04358434821a023b00772f9648
naeem <131764472+NEMO2257@users.noreply.github.com>
2023-12-07 16:09:05 +0000

    datatypes.js

--- 01_basic/02_ datatypes.js
+++ 01_basic/02_ datatypes.js
@@ -1,7 +1,19 @@
- "Use strict"; // treat all js acode as newer verson...
+"Use strict"; // treat all js acode as newer verson...
 
 
 //  alert(3 + 3) we are using nodejs ,note browser...
 
-console.log(3 + 3)
+console.log(3 + 3)//code readablity should be high...
 
+//number =>  2 to power 53
+//bigInt
+//string =>""
+//boolean =>true/false
+//null / standalone value
+//undefined
+//symbol  ..use after uniqe
+
+
+
+console.log(typeof null)//object
+console.log(typeof undefined)// already undefined.
